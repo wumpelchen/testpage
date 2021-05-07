9d7528384ee9b4462c7a49c04685fadfdff3f94f
Steve <wumpelchen@gmail.com>
2021-05-07 07:55:07 +0200

    reading test of the url params

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ console.log('läuft')
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString)
-console.log(urlParams.get('_id'))
+let _id = urlParams.get('_id')
+
+if(_id == null || _id === undefined) console.log('no _id found')
+else console.log(_id)
